598558635c84d04719390bcb76257d2be59b1077
Dylan j <dj0759@gmail.com>
2018-10-14 00:31:33 -0700

    First Commit

---
+++ ServiceExample/FTPTransfer.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Text;
+using System.Windows.Forms;
+using WinSCP;
+
+namespace ServiceExample
+{
+    public class FTPTransfer
+    {
+        protected string HostName { get; set; }
+        protected string UserName { get; set; }
+        protected string Password { get; set; }
+        protected string remoteDir { get; set; }
+        protected string localDestnDir { get; set; }
+
+        private void LogService(string content)
+        {
+            FileStream fs =
+                new FileStream(@"C:\Users\dj075\Documents\C#Reference\WindowsService\ServiceExample\Logs.txt",
+                    FileMode.OpenOrCreate, FileAccess.Write);
+            StreamWriter sw = new StreamWriter(fs);
+            sw.BaseStream.Seek(0, SeekOrigin.End);
+            sw.WriteLine(content);
+            sw.Flush();
+            sw.Close();
+        }
+
+        public FTPTransfer(string hostName, string userName, string password, string remoteDir, string localDir)
+        {
+            this.HostName = hostName;
+            this.UserName = userName;
+            this.Password = password;
+            this.remoteDir = remoteDir;
+            this.localDestnDir = localDir;
+        }
+
+        public void GetFilesFromFtp()
+        {
+            try
+            {
+                // Setup session options
+                SessionOptions sessionOptions = new SessionOptions
+                {
+                    // Protocol = protocol.Sftp
+                    Protocol = Protocol.Ftp,
+                    HostName = HostName,
+                    UserName = UserName,
+                    Password = Password
+                };
+
+                using (Session session = new Session())
+                {
+                    // Connect
+                    session.Open(sessionOptions);
+
+                    // Download files
+                    TransferOptions transferOptions = new TransferOptions();
+                    transferOptions.TransferMode = TransferMode.Binary;
+
+                    TransferOperationResult transferResult;
+                    transferResult =
+                        session.GetFiles(remoteDir, @localDestnDir, false, transferOptions);
+
+                    // Throw on any error
+                    transferResult.Check();
+
+                    // Print results
+                    foreach (TransferEventArgs transfer in transferResult.Transfers)
+                    {
+                        string status = "Download of "+ transfer.FileName + "succeeded at " + DateTime.Now;
+                        LogService(status);
+                    }
+                }
+
+                
+            }
+            catch (Exception e)
+            {
+                string status = DateTime.Now + " - Error: " + e;
+                LogService(status);
+                
+            }
+        }
+    }
+}
+      
---
+++ ServiceExample/TestService.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServiceExample
+{
+    public partial class TestService : ServiceBase
+    {
+        private System.Timers.Timer timeDelay;
+        private int count;
+        private DateTime currentDate;
+        private string logMessage;
+        public TestService()
+        {
+            InitializeComponent();
+            timeDelay = new System.Timers.Timer();
+            timeDelay.Elapsed += new System.Timers.ElapsedEventHandler(WorkProcess);
+
+        }
+
+        public void WorkProcess(object sender, System.Timers.ElapsedEventArgs e)
+        {
+
+            FTPTransfer FtpClient = new FTPTransfer("host", "username", "password", "/", "C:/TestFiles");
+            FtpClient.GetFilesFromFtp();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            currentDate = System.DateTime.Now;
+            logMessage = "Service Started - " + currentDate;
+            LogService(logMessage);
+            timeDelay.Enabled = true;
+        }
+
+        protected override void OnStop()
+        {
+            currentDate = System.DateTime.Now;
+            logMessage = "Service Stopped - " + currentDate;
+            LogService(logMessage);
+            timeDelay.Enabled = false;
+
+        }
+
+        // Writes status to file
+        private void LogService(string content)
+        {
+            FileStream fs =
+                new FileStream(@"C:\Users\dj075\Documents\C#Reference\WindowsService\ServiceExample\Logs.txt",
+                    FileMode.OpenOrCreate, FileAccess.Write);
+            StreamWriter sw = new StreamWriter(fs);
+            sw.BaseStream.Seek(0, SeekOrigin.End);
+            sw.WriteLine(content);
+            sw.Flush();
+            sw.Close();
+        }
+
+    }
+}
